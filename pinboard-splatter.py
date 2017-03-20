ab4f426654a9aee35a3657b3a6f4f2e137718e97
Rob Lanphier <robla@robla.net>
2017-03-20 08:57:34 -0700

    Ran autopep8 against pinboard-splatter.py

--- pinboard-splatter.py
+++ pinboard-splatter.py
@@ -7,35 +7,45 @@ import sys
 import codecs
 import locale
 import dateutil.parser
-import time, os
+import time
+import os
 import calendar
+
 
 def FIXMEOUT(string):
     print(string)
 
+
 # use preferred encoding, even when piping output to another program or file
 sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
+
 
 def get_link_status(url):
     return url
 
+
 def valid_link(status):
     return (status == requests.codes.ok)
-    
+
+
 def ignored_link(link, ignore_tags):
     ignore_tags = set(ignore_tags)
     link_tags = link['tags'].split(' ')
-    return True if ignore_tags.intersection(link_tags) else False 
+    return True if ignore_tags.intersection(link_tags) else False
+
 
 def splatter_summary_header():
     msg = '#Pinboard splatter results\n'
     return msg
 
+
 def splatter_summary_footer(num_links):
     return '\n%s Saved\n' % (num_links)
 
+
 def invalid_link_message(status, link):
     return '- Invalid link (%s): [%s](%s)  ' % (status, link['description'], link['href'])
+
 
 def save_link_json(link):
     import os
@@ -46,20 +56,25 @@ def save_link_json(link):
     linkdate = dateutil.parser.parse(link['time'])
     linktimetuple = time.mktime(linkdate.timetuple())
     os.utime(linkfilename, (linktimetuple, linktimetuple))
-    #FIXMEOUT(linkdate)
+    # FIXMEOUT(linkdate)
     return retval
-    #return '- [%s](%s)  ' % (link['description'], link['href'])
-    
+    # return '- [%s](%s)  ' % (link['description'], link['href'])
+
+
 def fix_date_on_file(linkfilename, linktimestring):
     linkdate = dateutil.parser.parse(linktimestring)
     linktime_local = calendar.timegm(linkdate.timetuple())
     os.utime(linkfilename, (time.time(), linktime_local))
     return linkdate
 
-def save_link_mime(link):
-    import os, copy
 
-    import email, sys, json
+def save_link_mime(link):
+    import os
+    import copy
+
+    import email
+    import sys
+    import json
     mimemsg = email.message_from_string("")
 
     headers = copy.copy(link)
@@ -72,7 +87,8 @@ def save_link_mime(link):
 
     linkdate = dateutil.parser.parse(link['time'])
     linkdatestr = time.strftime("%Y-%m-%d", linkdate.timetuple())
-    filename = os.path.join('hash', 'mime', linkdatestr + "-" + link['hash'][:7] + ".mime")
+    filename = os.path.join('hash', 'mime', linkdatestr +
+                            "-" + link['hash'][:7] + ".mime")
     with open(filename, 'wt') as f:
         f.write(mimemsg.as_string())
 
@@ -94,14 +110,16 @@ def process_links(links, ignore_tags):
     except KeyboardInterrupt:
         print "\nProcessing cancelled..."
         pass
-    
+
     print splatter_summary_footer(num_links_processed)
-        
-def process_bookmarks_file(filename, ignore_tags = []):
+
+
+def process_bookmarks_file(filename, ignore_tags=[]):
     with open(filename) as f:
         bookmarks = json.load(f)
         process_links(bookmarks, ignore_tags)
-        
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print 'Usage: pinboard-splatter.py <bookmarks.json>'
