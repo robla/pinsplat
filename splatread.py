9b11f25a5aa3cd0deaf5c12579a3ec4489f87dda
Rob Lanphier <robla@robla.net>
2017-04-05 12:02:50 -0700

    Backporting splatread.py to Python 2

--- splatread.py
+++ splatread.py
@@ -1,5 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 #import sys, argparse, fileinput, bs4
+from __future__ import with_statement
+from __future__ import absolute_import
 import argparse
 import email
 import fileinput
@@ -11,21 +13,24 @@ import string
 import struct
 import time
 import urllib
+import urlparse 
+
+from io import open
 
 
 def sha1_3char_abbr(x):
-    '''
+    u'''
     1. Make SHA-1 of the input string (x)
     2. Take the first couple of bytes
     3. base64 encode those
     The result should be 3 characters long
     '''
     # unpack assuming big endian
-    thishash = hashlib.sha1(x.encode('utf-8')).digest()
-    n = struct.unpack('>H', thishash[0:2])[0]
+    thishash = hashlib.sha1(x.encode(u'utf-8')).digest()
+    n = struct.unpack(u'>H', thishash[0:2])[0]
     # some code adapted from http://stackoverflow.com/questions/561486/
     ALPHABET = string.ascii_uppercase + string.ascii_lowercase + \
-        string.digits + '-_'
+        string.digits + u'-_'
     BASE = len(ALPHABET)
     s = []
     while True:
@@ -35,11 +40,11 @@ def sha1_3char_abbr(x):
             break
     while(len(s) < 3):
         s.append(ALPHABET[0])
-    return ''.join(reversed(s))
+    return u''.join(reversed(s))
 
 
 def get_domain_abbr(fqdn):
-    '''
+    u'''
     Make a unique-ish 8-letter domain abbreviation for a fully qualified
     domain name
 
@@ -52,52 +57,52 @@ def get_domain_abbr(fqdn):
     The goal is to balance uniqueness with readability and memorability.
     This is probably waaaaay overthought ;-)
     '''
-    if fqdn == 'localhost':
-        return 'localhst'
+    if fqdn == u'localhost':
+        return u'localhst'
 
-    domainbits = fqdn.split('.')
-    if domainbits[0] == 'www':
+    domainbits = fqdn.split(u'.')
+    if domainbits[0] == u'www':
         domainbits.pop(0)
-    if len(''.join(domainbits)) < 7 or len(domainbits[-1]) > 3:
-        domain = ''.join(domainbits)
-    elif len(''.join(domainbits[-2:-1])) < 7:
-        domain = ''.join(domainbits[-3:-1])
-    elif domainbits[-1] == 'com':
+    if len(u''.join(domainbits)) < 7 or len(domainbits[-1]) > 3:
+        domain = u''.join(domainbits)
+    elif len(u''.join(domainbits[-2:-1])) < 7:
+        domain = u''.join(domainbits[-3:-1])
+    elif domainbits[-1] == u'com':
         domain = domainbits[-2]
     else:
-        domain = ''.join(domainbits[-2:-1])
+        domain = u''.join(domainbits[-2:-1])
 
     pt1 = domain[0]
-    pt2 = ''.join([x for x in domain[1:-1] if x not in 'aeiou'])[0:3]
+    pt2 = u''.join([x for x in domain[1:-1] if x not in u'aeiou'])[0:3]
     pt3 = domain[-1]
     pt123 = pt1 + pt2 + pt3
     while(len(pt123) < 5):
-        pt123 += '-'
+        pt123 += u'-'
     pt4 = sha1_3char_abbr(fqdn)
     pt123 + pt4
     return pt123 + pt4
 
 
 def get_filename_from_mimemsg(mimemsg):
-    tmpurl = mimemsg['href']
+    tmpurl = mimemsg[u'href']
 
-    fqdn = urllib.parse.urlparse(tmpurl).netloc
-    scheme = urllib.parse.urlparse(tmpurl).scheme
-    if scheme == 'http' or scheme == 'https':
+    fqdn = urlparse.urlparse(tmpurl).netloc
+    scheme = urlparse.urlparse(tmpurl).scheme
+    if scheme == u'http' or scheme == u'https':
         domainabbr = get_domain_abbr(fqdn)
     else:
-        domainabbr = 'nodomain'
+        domainabbr = u'nodomain'
 
-    tmpdate = mimemsg['time']
+    tmpdate = mimemsg[u'time']
     ttup = dateutil.parser.parse(tmpdate).timetuple()
-    datestr = time.strftime("%Y%m%d", ttup)
+    datestr = time.strftime(u"%Y%m%d", ttup)
     timestr = get_base64_time(ttup)
-    shortsum = '-' + mimemsg['hash'][0:7]
-    retval = domainabbr + '-' + datestr + '-' + timestr + shortsum
+    shortsum = u'-' + mimemsg[u'hash'][0:7]
+    retval = domainabbr + u'-' + datestr + u'-' + timestr + shortsum
     if not len(retval) == 29:
-        print("retval: " + retval, file=sys.stderr)
-        print("len: " + str(len(retval)), file=sys.stderr)
-        raise ValueError("{} is {}/29 chars".format(retval, len(retval)))
+        print >>sys.stderr, u"retval: " + retval
+        print >>sys.stderr, u"len: " + unicode(len(retval))
+        raise ValueError(u"{} is {}/29 chars".format(retval, len(retval)))
 
     return retval
 
@@ -105,12 +110,12 @@ def get_filename_from_mimemsg(mimemsg):
 def get_base64_digit(x):
     # some code adapted from http://stackoverflow.com/questions/561486/
     ALPHABET = string.ascii_uppercase + string.ascii_lowercase + \
-        string.digits + '-_'
+        string.digits + u'-_'
     BASE = len(ALPHABET)
     r = x % BASE
     if x < 0 or x >= BASE:
         raise BaseException(
-            str(x) + ' is out of range to represent as single base64 digit')
+            unicode(x) + u' is out of range to represent as single base64 digit')
     return(ALPHABET[r])
 
 
@@ -121,43 +126,43 @@ def get_base64_time(ttup):
 
 
 def parse_arguments():
-    """ see http://docs.python.org/library/argparse """
+    u""" see http://docs.python.org/library/argparse """
     parser = argparse.ArgumentParser(
-        description='Print prettified htmlfile to stdout')
-    parser.add_argument("-n", "--nameonly", help="get the filename, not everything",
-        action="store_true")
-    parser.add_argument('htmlfile', help='raw html to clean up',
-                        nargs='?', default=None)
+        description=u'Print prettified htmlfile to stdout')
+    parser.add_argument(u"-n", u"--nameonly", help=u"get the filename, not everything",
+        action=u"store_true")
+    parser.add_argument(u'htmlfile', help=u'raw html to clean up',
+                        nargs=u'?', default=None)
     return parser.parse_args()
 
 
 def main(argv=None):
-    """ <http://www.crummy.com/software/BeautifulSoup/> prettify"""
+    u""" <http://www.crummy.com/software/BeautifulSoup/> prettify"""
 
     args = parse_arguments()
     if args.nameonly:
-        with open(args.htmlfile, 'r') as f:
+        with open(args.htmlfile, u'r') as f:
             splatstr = f.read()
         f.closed
         mimemsg = email.message_from_string(splatstr)
-        print(get_filename_from_mimemsg(mimemsg))
+        print get_filename_from_mimemsg(mimemsg)
         sys.exit()
 
-    splatstr = "".join(fileinput.input())
+    splatstr = u"".join(fileinput.input())
     mimemsg = email.message_from_string(splatstr)
 
-    tags = mimemsg['tags'].split(' ')
-    print('Filename: ' + get_filename_from_mimemsg(mimemsg))
-    print(' '.join(['@' + tag for tag in tags]))
-    print()
-    print(' '.join(['[[' + tag + ']]' for tag in tags]))
-    print()
-    print('[[' + mimemsg['href'] + '|' + mimemsg['description'] + ']]')
-    print(mimemsg['time'])
-    print()
-    print(mimemsg.get_payload())
+    tags = mimemsg[u'tags'].split(u' ')
+    print u'Filename: ' + get_filename_from_mimemsg(mimemsg)
+    print u' '.join([u'@' + tag for tag in tags])
+    print
+    print u' '.join([u'[[' + tag + u']]' for tag in tags])
+    print
+    print u'[[' + mimemsg[u'href'] + u'|' + mimemsg[u'description'] + u']]'
+    print mimemsg[u'time']
+    print
+    print mimemsg.get_payload()
 
 
-if __name__ == '__main__':
+if __name__ == u'__main__':
     exit_status = main(sys.argv)
     sys.exit(exit_status)
